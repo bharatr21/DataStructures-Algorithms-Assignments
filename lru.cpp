cddbaed77afe829efde2eaf1e9a03bd9a3eab05e
Bharat123rox <bharatraghunthan9767@gmail.com>
2018-07-24 18:59:16 +0530

    Add simple implementation of LRU cache
    LRU (Least Recently Used) is a page replacement policy for a limited frame size/cache size which deletes/flushes the least recently accessed page from the cache whenever there is a page fault and the cache is full.

---
+++ lru.cpp
@@ -0,0 +1,84 @@
+#include <bits/stdc++.h>
+#define ll long long
+#define pb push_back
+#define nl '\n'
+using namespace std;
+
+class LRU
+{
+	ll csize, pgf;
+	list<ll> dq;
+	unordered_map<ll, list<ll>::iterator> ma;
+public:
+	LRU()
+	{
+		csize = 3; //Default capacity of 3
+		pgf = 0;
+	}
+	LRU(ll cap)
+	{
+		csize = cap;
+		pgf = 0;
+	}
+	void refer(ll x)
+	{
+		if(ma.find(x) == ma.end())
+		{
+			cout << "Page Fault for: " << x << nl;
+			pgf++;
+			if(dq.size() == csize)
+			{
+				// Remove/Kick out the least recently used(LRU) entry
+				ma.erase(dq.back());
+				dq.pop_back();
+			}
+		}
+		else
+			dq.erase(ma[x]);
+		dq.push_front(x);
+		ma[x] = dq.begin();
+	}
+	void display()
+	{
+		cout << "Displaying the contents of the cache" << nl;
+		for(auto it:dq)
+		{
+			cout << it << ' ';
+		}
+		cout << nl;	
+	}
+	ll pagefaults()
+	{
+		return pgf;
+	}
+};
+
+int main()
+{
+	ll cap, t;
+	vector<ll> v;
+	cout << "Enter array of Pages: ";
+	while(cin.peek() != nl)
+	{
+		cin >> t;
+		v.pb(t);
+	}
+	cout << "Enter Page Frame Capacity: ";
+	try 
+	{
+		cin >> cap;
+		if(cap <= 0)
+			throw cap;
+	}
+	catch(ll x)
+	{
+		cout << "This is an invalid page cache" << nl;
+		cout << "Cannot have " << x << " pages in frame" << nl;
+		return 0;
+	}
+	LRU lru = LRU(cap);
+	for(auto u:v)  {lru.refer(u);}
+	lru.display();
+	cout << "Number of pagefaults: " << lru.pagefaults() << nl;
+	return 0;
+}
